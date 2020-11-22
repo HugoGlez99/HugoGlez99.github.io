42225df20f0471757195fdcae4a94f7b917a596a
HugoGlez99 <61038026+HugoGlez99@users.noreply.github.com>
2020-11-22 12:46:45 -0600

    formulafoother y manifest

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,7 +1,7 @@
 class MiFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = /* html */
-      `Copyright &copy; Hugo Alberto Glez`;
+      `Copyright &copy; Hugo Alberto Gonzalez Perez`;
   }
 }
 customElements.define("mi-footer", MiFooter);
